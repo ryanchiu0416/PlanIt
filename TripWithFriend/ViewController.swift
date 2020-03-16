09daa14e9a00adb3d86f9b4863ca49a491e01410
Ryan Chiu <ryanchiu@uw.edu>
2020-03-16 16:10:57 -0700

    Initial Commit

---
+++ TripWithFriend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripWithFriend
+//
+//  Created by Ryan Chiu on 2020/3/16.
+//  Copyright © 2020 Ryan Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
